391cd65f667204dbe7052c08e9c78f1a1f98f63a
arjenmarkus <arjenmarkus@6bca0825-fc2c-4065-8091-e7187c906985>
2008-03-25 05:23:36 +0000

    Adding various reporting modules (writing HTML. Latex, RTF files, logging and handling exceptions/assertions)

--- trunk/tests/strings/test_manip.f90
+++ trunk/tests/strings/test_manip.f90
@@ -1,7 +1,7 @@
 ! test_manip.f90 --
 !     Program to test the string manipulation routines
 !
-!     $Id: test_manip.f90,v 1.1 2006-06-23 08:04:13 arjenmarkus Exp $
+!     $Id: test_manip.f90,v 1.2 2008-03-25 05:23:36 arjenmarkus Exp $
 !
 program test_manip
     use string_manipulation
@@ -24,4 +24,33 @@ program test_manip
        call random_word( string )
        write(*,*) '    >', string, '<'
     enddo
+
+    write(*,*) 'Insert/delete/replace substrings:'
+
+    test_line = 'ABCDEFGH'
+    write(*,*) 'String:      >',test_line,'<'
+    call string_insert( test_line, 1, 'abc' )
+    write(*,*) 'Insert at 1: >',test_line,'<'
+    test_line = 'ABCDEFGH'
+    call string_insert( test_line, 2, 'abc' )
+    write(*,*) 'Insert at 2: >',test_line,'<'
+
+    test_line = 'ABCDEFGH'
+    call string_delete( test_line, 1, 2 )
+    write(*,*) 'Delete at 1: >',test_line,'<'
+    test_line = 'ABCDEFGH'
+    call string_delete( test_line, 2, 2 )
+    write(*,*) 'Delete at 2: >',test_line,'<'
+
+    test_line = 'ABCDEFGH'
+    call string_replace( test_line, 'AB', 'abc' )
+    write(*,*) 'Replace AB: >',test_line,'<'
+    test_line = 'ABCDEFGH'
+    call string_replace( test_line, 'XY', 'abc' )
+    write(*,*) 'Replace XY: >',test_line,'<'
+
+    test_line = 'ABABABBA'
+    call string_map( test_line, 'AB', 'abc' )
+    write(*,*) 'Map AB:     >',test_line,'<'
+
 end program test_manip
