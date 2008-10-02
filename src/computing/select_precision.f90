0a39d1045043feddbc23ac82ba94685e95ca8d57
arjenmarkus <arjenmarkus@6bca0825-fc2c-4065-8091-e7187c906985>
2008-10-02 09:02:33 +0000

    Various updates

--- src/computing/select_precision.f90
+++ src/computing/select_precision.f90
@@ -6,13 +6,19 @@
 !
 !     wp: kind for working precision (set to either sp or dp)
 !
-!     $Id: select_precision.f90,v 1.2 2006-03-26 19:03:53 arjenmarkus Exp $
+!     Note:
+!     Originally the kind for double precision was determined by
+!     selecting a precision strictly larger than single precision.
+!     But what if via compiler options single precision constants
+!     are promoted to double precision?
+!
+!     $Id: select_precision.f90,v 1.3 2008-10-02 09:02:33 arjenmarkus Exp $
 !
 module select_precision
     implicit none
 
     integer, parameter :: sp = kind( 1.0 )
-    integer, parameter :: dp = selected_real_kind( precision( 1.0 ) + 1 )
+    integer, parameter :: dp = kind( 1.0d0 )
 
     integer, parameter :: wp = sp
 
